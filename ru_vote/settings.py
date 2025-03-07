d18b91f9c2b3465e606594ba323ce9fc0b7f1cd0
salvatore-droid <geniusokwemba53@gmail.com>
2025-03-07 17:42:55 +0300

    added a requirements.txt file

--- ru_vote/settings.py
+++ ru_vote/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*0!&_%pn%7#!38l76h37e$86k)%=akpn41fl49bbx!7#nf1js&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
